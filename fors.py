9bd113c7bc637b971ab74dc2caa63e0357a3e484
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-02 05:02:52 +0100

    Add files via upload

--- fors.py
+++ fors.py
@@ -1,17 +1,13 @@
+import copy
 def process(a:int):
-     print(a)
+    print(a)
+def fors(procs,frommm,intos,steeps):
 
-
-def fors(_restarts,procs):
     
-    _v=vars()
-    _v.update(dict(_restarts))
     
     while frommm < intos:
-         procs(frommm)
-         frommm=frommm+steeps
-
+        procs(frommm)
+        frommm=frommm+steeps
 _restarts={'frommm':0 ,'intos':10 , 'steeps':1 }
-_v=vars()
-_v.update(_restarts)
-fors(_restarts,process)
+
+fors(process,**_restarts)
